a1ac751eb91150639c95f988f842b90f4bd797b6
ladm99 <ladelmar99@gmail.com>
2023-01-30 16:07:30 -0600

    added Repositories

---
+++ assessment1/src/main/java/com/cooksys/assessment1/repositories/TweetRepository.java
@@ -0,0 +1,10 @@
+package com.cooksys.assessment1.repositories;
+
+import com.cooksys.assessment1.entities.Tweet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
+    //    TODO: Add derived queries if necessary
+}
