320892c5927ec1d22ac43ab1afeaf10e829c0390
mutotu <muttalip.aydemir@gmail.com>
2023-01-31 10:49:20 -0800

    removed the entties in hastag mapper

--- assessment1/src/main/java/com/cooksys/assessment1/mappers/HashTagMapper.java
+++ assessment1/src/main/java/com/cooksys/assessment1/mappers/HashTagMapper.java
@@ -1,14 +1,10 @@
 package com.cooksys.assessment1.mappers;
 
-import com.cooksys.assessment1.entities.Hashtag;
-import com.cooksys.assessment1.entities.Tweet;
 import org.mapstruct.Mapper;
-
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface HashTagMapper {
-    List<Tweet> requestDtoEntity(Hashtag hashtag);
+            //add entities
 
 
 }
