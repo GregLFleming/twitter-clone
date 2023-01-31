03bf595b0c4b719321f63cea195c86258540cc1e
mutotu <muttalip.aydemir@gmail.com>
2023-01-31 08:59:38 -0800

    added @nonmull to the fiels

--- assessment1/src/main/java/com/cooksys/assessment1/dtos/CredentialsRequestDTO.java
+++ assessment1/src/main/java/com/cooksys/assessment1/dtos/CredentialsRequestDTO.java
@@ -2,10 +2,13 @@ package com.cooksys.assessment1.dtos;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @NoArgsConstructor
 @Data
 public class CredentialsRequestDTO {
+    @NonNull
     private String username;
+    @NonNull
     private String password;
 }
