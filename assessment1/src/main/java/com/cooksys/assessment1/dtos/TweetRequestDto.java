236530a9da77797940296a99c1848f34486cff0f
ladm99 <ladelmar99@gmail.com>
2023-01-31 12:05:09 -0600

    added TweetRequestDto

---
+++ assessment1/src/main/java/com/cooksys/assessment1/dtos/TweetRequestDto.java
@@ -0,0 +1,11 @@
+package com.cooksys.assessment1.dtos;
+
+import com.cooksys.assessment1.entities.Credentials;
+
+public class TweetRequestDto {
+
+    private String content;
+
+//    private CredentialsDto credentials;
+
+}
