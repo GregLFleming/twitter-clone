efe7f3e340be5673c700c3e6755d12d32d601848
ladm99 <ladelmar99@gmail.com>
2023-01-31 12:12:24 -0600

    added TweetResponseDto

--- assessment1/src/main/java/com/cooksys/assessment1/dtos/TweetRequestDto.java
+++ assessment1/src/main/java/com/cooksys/assessment1/dtos/TweetRequestDto.java
@@ -1,8 +1,13 @@
 package com.cooksys.assessment1.dtos;
 
 import com.cooksys.assessment1.entities.Credentials;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class TweetRequestDto {
+
 
     private String content;
 
