cd91661d8fc8103ba1504a9f2f6e506d814dcb02
Gregory Fleming <flemdog13@hotmail.com>
2023-02-01 17:32:42 -0800

    error testing

--- assessment1/src/main/java/com/cooksys/assessment1/services/impl/UserServiceImpl.java
+++ assessment1/src/main/java/com/cooksys/assessment1/services/impl/UserServiceImpl.java
@@ -3,16 +3,13 @@ package com.cooksys.assessment1.services.impl;
 import java.util.List;
 import java.util.Optional;
 
-import com.cooksys.assessment1.dtos.UserRequestDto;
-import com.cooksys.assessment1.exceptions.BadRequestException;
-import com.cooksys.assessment1.mappers.CredentialsMapper;
-import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import com.cooksys.assessment1.dtos.CredentialsDto;
 import com.cooksys.assessment1.dtos.UserRequestDto;
 import com.cooksys.assessment1.dtos.UserResponseDto;
 import com.cooksys.assessment1.entities.Credentials;
+import com.cooksys.assessment1.entities.Profile;
 import com.cooksys.assessment1.entities.User;
 import com.cooksys.assessment1.exceptions.BadRequestException;
 import com.cooksys.assessment1.exceptions.NotFoundException;
@@ -31,19 +28,71 @@ public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
     private final CredentialsMapper credentialsMapper;
 
-
-	private void validateUserRequest(UserRequestDto userRequestDto){
-
+    private void validateUserRequest(UserRequestDto userRequestDto){
+    	if(userRequestDto.getCredentials() == null || userRequestDto.getProfile() == null) {
+    		throw new BadRequestException("Request body missing either credentials or profile.");
+    	}
 		if(userRequestDto.getProfile().getEmail() == null || userRequestDto.getCredentials().getPassword() == null || userRequestDto.getCredentials().getUsername() == null){
 			throw new BadRequestException("Request body missing required fields");
 		}
 
 	}
-
-	@Override
+    
+    @Override
 	public List<UserResponseDto> getUsers() {
 		return userMapper.entitiesToResponseDTOs(userRepository.findAllByDeletedFalse());
 	}
+    
+	@Override
+	public UserResponseDto createUser(UserRequestDto userRequestDto) {
+		validateUserRequest(userRequestDto);
+
+		User user = userMapper.requestDtoToEntity(userRequestDto);
+		Optional<User> check = userRepository.findByCredentials(user.getCredentials());
+		if(!check.isEmpty())
+			throw new BadRequestException("User with this username already exists");
+
+		if(!check.isEmpty() && check.get().isDeleted()) {
+			user = check.get();
+			user.setDeleted(false);
+		}
+		return userMapper.entityToResponseDto(userRepository.saveAndFlush(user));
+	}
+
+	@Override
+	public UserResponseDto updateUsername(UserRequestDto userRequestDto, String username) {
+		
+		//check for missing profile
+		if(userRequestDto.getProfile() == null) {
+			throw new BadRequestException("The request body must contain a profile");
+		}
+		
+		//check if user exists
+		Optional<User> queryResult = userRepository.findByCredentials(userMapper.userRequestDtoToEntity(userRequestDto).getCredentials());
+		if(queryResult.isEmpty()) {
+			throw new NotFoundException("Your credentials cannot be found in the database");
+		}
+		User user = queryResult.get();
+		
+		//check if user is deleted
+		if(user.isDeleted()) {
+			throw new BadRequestException("Username " + user.getCredentials().getUsername() + " has been deleted.");
+		}
+
+		//update username
+		Credentials newCredentials = user.getCredentials();
+		newCredentials.setUsername(username);
+		user.setCredentials(newCredentials);
+		
+//		//update profile
+//		Profile newProfile = userMapper.requestDtoToEntity(userRequestDto).getProfile();
+//		user.setProfile(newProfile);
+		
+		//save to database
+		return userMapper.entityToResponseDto(userRepository.saveAndFlush(user));
+	}
+	
+	
 
 	@Override
 	public void followUser(CredentialsDto credentialsRequestDto, String username) {
@@ -126,46 +175,7 @@ public class UserServiceImpl implements UserService {
 		return;
 	}
 
-	@Override
-	public UserResponseDto updateUsername(UserRequestDto userRequestDto, String username) {
-		
-		//check if user exists
-		Optional<User> queryResult = userRepository.findByCredentials(userMapper.userRequestDtoToEntity(userRequestDto).getCredentials());
-		if(queryResult.isEmpty()) {
-			throw new NotFoundException("Your credentials cannot be found in the database");
-		}
-		User user = queryResult.get();
-		
-		//check if user is deleted
-		if(user.isDeleted()) {
-			throw new BadRequestException("Username " + user.getCredentials().getUsername() + " has been deleted.");
-		}
-
-		//update username
-		Credentials newCredentials = user.getCredentials();
-		newCredentials.setUsername(username);
-		user.setCredentials(newCredentials);
-		
-		//save to database
-		return userMapper.entityToResponseDto(userRepository.saveAndFlush(user));
-	}
-
-	@Override
-	public UserResponseDto createUser(UserRequestDto userRequestDto) {
-		validateUserRequest(userRequestDto);
-
-		User user = userMapper.requestDtoToEntity(userRequestDto);
-		Optional<User> check = userRepository.findByCredentials(user.getCredentials());
-		if(!check.isEmpty())
-			throw new BadRequestException("User with this username already exists");
-
-		if(!check.isEmpty() && check.get().isDeleted()) {
-			user = check.get();
-			user.setDeleted(false);
-		}
+	
 
 
-
-		return userMapper.entityToResponseDto(userRepository.saveAndFlush(user));
-	}
 }
